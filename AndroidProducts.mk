4e3caa018e2554b932a3cd4453dcebd0c036ab18
Sourajit Karmakar <sourajitk@statixos.com>
2022-10-06 22:53:34 -0400

    coral: Add statix_flame targets

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,12 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_coral.mk \
     $(LOCAL_DIR)/aosp_flame.mk \
+    $(LOCAL_DIR)/statix_flame.mk \
     $(LOCAL_DIR)/aosp_coral_hwasan.mk \
     $(LOCAL_DIR)/aosp_flame_hwasan.mk \
 
 COMMON_LUNCH_CHOICES := \
     aosp_coral-userdebug \
     aosp_flame-userdebug \
+    statix_flame-userdebug \
+    statix_flame-user
---
+++ statix_flame.mk
@@ -0,0 +1,19 @@
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+# Parts
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/coral/aosp_flame.mk)
+
+# Define product.
+PRODUCT_NAME := statix_flame
+PRODUCT_MODEL := Pixel 4
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="flame-user 13 TP1A.221005.002 9012097 release-keys"
+
+BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGERPRINT)
